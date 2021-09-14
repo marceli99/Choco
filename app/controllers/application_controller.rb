623b31921eda46f4b5fd7f2dc6ecc989bbf101e1
Marcel <marceli99@protonmail.com>
2021-09-14 19:19:47 +0200

    cleanup code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,13 @@
 class ApplicationController < ActionController::Base
+
   def index
     @posts = Post.order('created_at DESC')
   end
-  # Devise runtime generated helpers
-  def current_user
-    super
-  end
-  def user_signed_in?
-    super
-  end
-  def destroy_user_session_path
-    super
-  end
-  def new_user_session_path
-    super
-  end
+
   def authenticate_user!
     unless current_user
       redirect_to :root
     end
   end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
   MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::SmartyHTML)
 
-  before_action :authenticate_user!, except: [:show]
   def index
     @posts = Post.order('created_at DESC')
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     @post.content = MARKDOWN.render(@post.content)
     @post.save
     redirect_to root_path
   end
+
   def edit
     @post = Post.find(params[:id])
   end
+
   def update
     @post = Post.find(params[:id])
     @post.content = MARKDOWN.render(params[:post][:content])
@@ -24,14 +29,17 @@ class PostsController < ApplicationController
     @post.save
     redirect_to root_path
   end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   private
   def post_params
     params.require(:post).permit(:id, :title, :content, :image)
