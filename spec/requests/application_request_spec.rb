3442367c4568eb523f2da236a211877df3fabf6e
Marcel Hasiński <marcel.hasinski@programa.pl>
2021-09-19 19:26:28 +0200

    test: add more tests

--- spec/requests/application_request_spec.rb
+++ spec/requests/application_request_spec.rb
@@ -1,10 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Applications", type: :request do
-    describe "GET index" do
+
+  describe "Guest" do
+    describe "GET /" do
       it "has a 200 status code" do
-        get '/'
+        get root_path
         expect(response.status).to eq(200)
       end
+      it "has a links for unsigned user" do
+        get root_path
+        expect(response.body).to include(root_path)
+        expect(response.body).to include(login_path)
+      end
     end
+  end
+
+  describe "User" do
+    let!(:user) { FactoryBot.create(:user) }
+    before { sign_in(user) }
+    describe "GET /" do
+      it "has a 200 status code" do
+        get root_path
+        expect(response.status).to eq(200)
+      end
+      it "has a links for signed user" do
+        get root_path
+        expect(response.body).to include(root_path)
+        expect(response.body).to include(new_post_path)
+        expect(response.body).to include(posts_path)
+        expect(response.body).to include(destroy_user_session_path)
+      end
+    end
+  end
+
 end
